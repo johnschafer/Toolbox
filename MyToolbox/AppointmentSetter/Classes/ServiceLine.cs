72aa8a59081324a47655cd7e2594ee9b52c731df
johnschafer <dukeschafer@gmail.com>
2017-08-22 03:43:48 -0400

    Feature (#4)
    * Welcome statement.
    
    * First step make tests fail.
    
    * Next we make the tests pass.
    
    * Added Appointment setter example. Demonstration of using web forms and unit tests.
    
    * Fixed not adding the projects correctly

---
+++ MyToolbox/AppointmentSetter/Classes/ServiceLine.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AppointmentSetter.Classes
+{
+    public class ServiceLine
+    {
+        public DateTime startTime;
+        public DateTime endTime;
+        public string service;
+    }
+}
